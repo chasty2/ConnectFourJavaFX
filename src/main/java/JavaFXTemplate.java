6a12877df742bbe4671badc98707b3eb9092161c
Cody Hasty <chasty2@vorel.localdomain>
2021-02-28 16:10:11 -0600

    commit 2

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -1,9 +1,10 @@
 import javafx.application.Application;
-
+import javafx.scene.control.Button;
 import javafx.scene.Scene;
-
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
 
 public class JavaFXTemplate extends Application {
 
@@ -18,12 +19,43 @@ public class JavaFXTemplate extends Application {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("Welcome to JavaFX");
 		
-		
+		Scene g = createGameScene();
 		
 				
-		Scene scene = new Scene(new VBox(), 700,700);
-		primaryStage.setScene(scene);
+		
+		primaryStage.setScene(g);
 		primaryStage.show();
 	}
-
-}
+	
+	public Scene createGameScene()
+	{
+		// Mybu
+		//MyButton arr[][] = ;
+		// Buttons used by menu Hbox
+		Button revBut = new Button("Reverse");
+		Button themeBut = new Button("Themes");
+		Button newGameBut = new Button ("Start New Game");
+		Button exitBut = new Button("Exit");
+		// menu Hbox
+		HBox menu = new HBox(10,revBut,themeBut,newGameBut,exitBut);
+		
+		// Grid Pane for the board
+		GridPane board = new GridPane();
+		// Add grid buttons
+		int x = 0; 
+		int y = 0;
+		for (x = 0; x < 7; x++) {
+			for (y = 0; y < 6; y++) {
+				MyButton b = new MyButton(x, y);
+				b.setMinSize(40, 40);
+				b.setStyle("-fx-background-color: lightgrey;");
+				board.add(b, x, y);
+			}
+		}
+		// Populate and return gameScene 
+		Scene gameScene = new Scene(board, 700,700);
+		return gameScene;
+	}
+	
+	// End JavaFXTemplate
+}	
---
+++ src/main/java/MyButton.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.function.Function;
+import javafx.animation.PauseTransition;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class MyButton extends Button{
+	private int x;
+	private int y;
+	
+	public MyButton(int newX, int newY) {
+		x = newX;
+		y = newY;
+	}
+}
